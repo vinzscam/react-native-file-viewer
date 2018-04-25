59617123d711b5a746db16be6649587c61ad041f
Dawid Misiło <misieekq@gmail.com>
2018-04-25 12:26:09 +0200

    add windows support

---
+++ windows/RNFileViewer/RNFileViewerModule.cs
@@ -0,0 +1,55 @@
+﻿using ReactNative.Bridge;
+using Windows.Storage;
+using Windows.System;
+using System;
+
+namespace RNFileViewer
+{
+    /// <summary>
+    /// A module that allows JS to share data.
+    /// </summary>
+    class RNFileViewerModule : NativeModuleBase
+    {
+        /// <summary>
+        /// The name of the native module.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return "RNFileViewer";
+            }
+        }
+
+        [ReactMethod]
+        public async void open(string filepath, string _, IPromise promise)
+        {
+            try
+            {
+                var file = await StorageFile.GetFileFromPathAsync(filepath);
+
+                if (file != null)
+                {
+                    var success = await Launcher.LaunchFileAsync(file);
+
+                    if (success)
+                    {
+                        promise.Resolve(null);
+                    }
+                    else
+                    {
+                        promise.Reject(null, "File open failed.");
+                    }
+                }
+                else
+                {
+                    promise.Reject(null, "File not found.");
+                }
+            } catch (Exception e)
+            {
+                promise.Reject(null, filepath, e);
+            }
+            
+        }
+    }
+}
---
+++ windows/RNFileViewer/RNFileViewerPackage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace RNFileViewer
+{
+    /// <summary>
+    /// Package defining core framework modules (e.g., <see cref="UIManagerModule"/>).
+    /// It should be used for modules that require special integration with
+    /// other framework parts (e.g., with the list of packages to load view
+    /// managers from).
+    /// </summary>
+    public class RNFileViewerPackage : IReactPackage
+    {
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>
+            {
+                new RNFileViewerModule()
+            };
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers(ReactContext reactContext)
+        {
+            return new List<IViewManager>(0);
+        }
+    }
+}
