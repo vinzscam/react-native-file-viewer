c376aa9f63304eee38668dffe97e3bd5c8d0870f
vinzscam <vinz.scamporlino@gmail.com>
2020-05-14 21:37:42 +0200

    Decode uri automatically on iOS

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
-import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import { NativeEventEmitter, NativeModules, Platform } from "react-native";
 
 const { RNFileViewer } = NativeModules;
 const eventEmitter = new NativeEventEmitter(RNFileViewer);
 
 let lastId = 0;
 
-function open(path, options = { }) {
-  const _options = (typeof options === 'string')
-    ? { displayName: options }
-    : options;
+function open(path, options = {}) {
+  const _options =
+    typeof options === "string" ? { displayName: options } : options;
   const { onDismiss, ...nativeOptions } = _options;
 
-  if (!['android', 'ios'].includes(Platform.OS)) {
+  if (!["android", "ios"].includes(Platform.OS)) {
     return RNFileViewer.open(path, nativeOptions);
   }
 
   return new Promise((resolve, reject) => {
     const currentId = ++lastId;
 
-    const openSubscription = eventEmitter.addListener('RNFileViewerDidOpen', ({ id, error }) => {
-      if(id === currentId) {
-        openSubscription.remove();
-        return error ? reject(new Error(error)) : resolve();
+    const openSubscription = eventEmitter.addListener(
+      "RNFileViewerDidOpen",
+      ({ id, error }) => {
+        if (id === currentId) {
+          openSubscription.remove();
+          return error ? reject(new Error(error)) : resolve();
+        }
       }
-    });
-    const dismissSubscription = eventEmitter.addListener('RNFileViewerDidDismiss', ({ id }) => {
-      if(id === currentId) {
-        dismissSubscription.remove();
-        onDismiss && onDismiss();
+    );
+    const dismissSubscription = eventEmitter.addListener(
+      "RNFileViewerDidDismiss",
+      ({ id }) => {
+        if (id === currentId) {
+          dismissSubscription.remove();
+          onDismiss && onDismiss();
+        }
       }
-    });
+    );
 
-    RNFileViewer.open(path, currentId, nativeOptions);
+    RNFileViewer.open(normalize(path), currentId, nativeOptions);
   });
+}
+
+function normalize(path) {
+  if (Platform.OS === "ios") {
+    const filePrefix = "file://";
+    if (path.startsWith(filePrefix)) {
+      path = path.substring(filePrefix.length);
+      try {
+        path = decodeURI(path);
+      } catch (e) {}
+    }
+  }
+  return path;
 }
 
 export default { open };
